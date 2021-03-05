165ecbe770837d1c7940e677bc69571840d363d4
gpyne5 <w5000c2d26@gmail.com>
2021-03-05 10:43:29 +0900

    index.vue CarsTable.vueの修正 (laravel) CorsMiddlewareをグローバルで有効に

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,3 +1,5 @@
+export { default as CarsTable } from '../../adminboard/components/CarsTable.vue'
 export { default as Logo } from '../../adminboard/components/Logo.vue'
 
+export const LazyCarsTable = import('../../adminboard/components/CarsTable.vue' /* webpackChunkName: "components/cars-table" */).then(c => c.default || c)
 export const LazyLogo = import('../../adminboard/components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c)
--- .nuxt/components/plugin.js
+++ .nuxt/components/plugin.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 
 const components = {
+  CarsTable: () => import('../../adminboard/components/CarsTable.vue' /* webpackChunkName: "components/cars-table" */).then(c => c.default || c),
   Logo: () => import('../../adminboard/components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c)
 }
 
