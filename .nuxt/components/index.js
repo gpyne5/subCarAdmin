2df83f73464fee10bb1171c0d33584528d20a76e
gpyne5 <w5000c2d26@gmail.com>
2021-03-05 11:23:34 +0900

    CurrentMonthを追加

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,5 +1,7 @@
 export { default as CarsTable } from '../../adminboard/components/CarsTable.vue'
+export { default as CurrenMonth } from '../../adminboard/components/CurrenMonth.vue'
 export { default as Logo } from '../../adminboard/components/Logo.vue'
 
 export const LazyCarsTable = import('../../adminboard/components/CarsTable.vue' /* webpackChunkName: "components/cars-table" */).then(c => c.default || c)
+export const LazyCurrenMonth = import('../../adminboard/components/CurrenMonth.vue' /* webpackChunkName: "components/curren-month" */).then(c => c.default || c)
 export const LazyLogo = import('../../adminboard/components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c)
--- .nuxt/components/plugin.js
+++ .nuxt/components/plugin.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 const components = {
   CarsTable: () => import('../../adminboard/components/CarsTable.vue' /* webpackChunkName: "components/cars-table" */).then(c => c.default || c),
+  CurrenMonth: () => import('../../adminboard/components/CurrenMonth.vue' /* webpackChunkName: "components/curren-month" */).then(c => c.default || c),
   Logo: () => import('../../adminboard/components/Logo.vue' /* webpackChunkName: "components/logo" */).then(c => c.default || c)
 }
 
